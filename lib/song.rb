95319d563df9d17455c071cb5b30fc006d9eaf1f
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-24 14:43:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,18 @@ class Song
     end
     hash 
   end 
-
+  
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] += 1
+      else
+        hash[artist] = 1
+      end 
+    end
+    hash 
+  end
+  
 end
   
